eeeba1f2aae745b8249b8ab478f7f155b5a9d32d
Prg-maker <df3296709@gmail.com>
2021-11-13 20:01:13 -0300

    add new config

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../prisma"
 import { io} from "../app"
+import {v4 as uuid } from 'uuid' 
 
 class CreateMessageService{
   async execute(text: string , user_id: string){
@@ -12,8 +13,9 @@ class CreateMessageService{
         user:true
       }
     })
-
+    
     const info = {
+      id: uuid(),
       message: message.text,
       user_id: message.user_id,
       created_at: message.created_at,
@@ -22,6 +24,7 @@ class CreateMessageService{
         avatar_url: message.user.avatar_url
       }
     }
+    console.log(message.text)
 
     io.emit("new_message" , info)
 
